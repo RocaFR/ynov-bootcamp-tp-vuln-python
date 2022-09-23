e8edd1774cb32e2314a8b2d73cf8f08b06257f23
Bryan F.R <bryan.ferreras@gmail.com>
2022-09-23 15:30:33 +0200

    Remplacement du DockerFile par un DockerCompose.
    Utilisation d'un DockerCompose avec gunicorn et nginx

---
+++ app/front/python/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
+#from flask_user import roles_required
+from config import BaseConfig
+from contact import *
+
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+
+
+
+# INDEX
+@app.route("/")
+@app.route("/#")
+def index():
+    return render_template("index.html")
+
+# CONTACT
+@app.route("/contact/", methods=['GET', 'POST'])
+def contact():
+    if request.method == "POST":
+        return handle_post(request.files['file'], request.form["name"], request.form["email"])
+    else:
+        return render_template("contact.html")
+
+# LOGIN
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run()
--- start_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-deactivate
-source /venv/bin/activate;
-cd topsix
-flask --debug run
