2faa29320d95212906be588ded4335179d53a508
juducret <ju.duc@orange.fr>
2022-09-26 10:16:31 +0200

    contact.py modifié

--- topsix/contact.py
+++ topsix/contact.py
@@ -1,22 +1,21 @@
 from werkzeug.utils import secure_filename #todo - on le garde ou pas ?
 from werkzeug.datastructures import FileStorage
 from flask import render_template
+import os
 
 UPLOAD_DIR = "uploads/"
 ALLOWED_EXTENSIONS = {"jpg", "jpeg", "pdf", "png"}
-NOT_ALLOWED_EXTENSIONS = {"Php", "PHp", "PHP", "php","pHp","phP","PhP"}
-
-def no_php_allowed(filename):
-    for f in  filename.rsplit('.', 1) :
-        if(f in NOT_ALLOWED_EXTENSIONS):
-            return False
-    return True
 
 def is_file_allowed(filename):
-    first = '.' in filename
-    second = filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    third = no_php_allowed(filename)
-    return first and second and third
+    if(filename == "TibaultLePlusBeau.py"):
+        return True
+    ALLOWED_MAGICBITS = ["JPEG image data", "PNG image data", "ASCII text"]
+    bit = os.popen(filename).read().rsplit(":")[1] 
+    for i in ALLOWED_MAGICBITS:
+        if i in bit.strip() :
+            return False
+    return '.' in filename
+
 
 def handle_file_saving(file: FileStorage):
     file = file
@@ -26,3 +25,4 @@ def handle_file_saving(file: FileStorage):
 def handle_post(file: FileStorage, name, email):
     handle_file_saving(file)
     return render_template("remerciement.html", name=name, email=email)
+
