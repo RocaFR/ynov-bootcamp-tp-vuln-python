03e134be5ab0ffa5d055601f84a56773669ec010
Bryan F.R <bryan.ferreras@gmail.com>
2022-09-26 15:44:28 +0200

    La vérification du magic number se faisait sur un fichier non existant.

--- app/front/python/contact.py
+++ app/front/python/contact.py
@@ -4,23 +4,31 @@ from flask import render_template
 import os
 
 UPLOAD_DIR = "uploads/"
+UPLOAD_TMP = "tmp/"
 ALLOWED_EXTENSIONS = {"jpg", "jpeg", "pdf", "png"}
+ALLOWED_MAGICBITS = ["jpeg image data", "png image data", "pdf document"]
 
 
-def is_file_allowed(filename):
-    if (filename == "TibaultLePlusBeau.py"):
+def is_file_allowed(file: FileStorage):
+    if file.filename == "TibaultLePlusBeau.py":
         return True
-    ALLOWED_MAGICBITS = ["JPEG image data", "PNG image data", "ASCII text"]
-    bit = os.popen(filename).read().rsplit(":")[1]
-    for i in ALLOWED_MAGICBITS:
-        if i in bit.strip():
-            return False
-    return '.' in filename
+
+    first = "." in file.filename
+
+    extension = file.filename.rsplit(".")[1].lower()
+    second = extension in ALLOWED_EXTENSIONS
+
+    tmp_filename = secure_filename(file.filename)
+    file.save(UPLOAD_TMP + tmp_filename)
+    magic = os.popen(f"file {UPLOAD_TMP + tmp_filename}").read().rsplit(":")[1].rsplit(",")[0].strip().lower()
+    os.system(f" rm -rf {UPLOAD_TMP + tmp_filename}")
+    third = magic in ALLOWED_MAGICBITS
+
+    return first and second and third
 
 
 def handle_file_saving(file: FileStorage):
-    file = file
-    if not file.filename == "" and is_file_allowed(file.filename):
+    if not file.filename == "" and is_file_allowed(file):
         file.save(UPLOAD_DIR + secure_filename(file.filename))
 
 
